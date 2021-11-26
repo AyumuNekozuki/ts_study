d1795b1c4b521b49d4201184bf526f9438c41e1e
AyumuNekozuki <ayumu@nekozuki.me>
2021-11-26 19:50:53 +0900

    statusup

---
+++ sec2/index.js
@@ -0,0 +1,24 @@
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+var double = "hello";
+var back = "hello";
+var hello;
+hello = 'hello';
+var person = {
+    name: 'Jack',
+    age: 21
+};
+var person2 = {
+    // const person2: {} ={
+    name: 'jack',
+    age: 21
+};
+// console.log(person2.name); //エラーになる
+var fruits = ['Apple', 'Banana', 'Grape'];
+var fruit = fruits[0];
+var book = ['business', 1500, false];
+// book.push(21);  // 追加時エラーは出ない
+// console.log(book[3]); //参照時にエラーをだす
--- sec2/index.ts
+++ sec2/index.ts
@@ -1,9 +1,37 @@
-let hasValue: boolean = true;
+let hasValue = true;
 
-let count: number = 10;
-let float: number = 3.14;
-let negative: number = -0.12;
+let count = 10;
+let float = 3.14;
+let negative = -0.12;
 
-let single: string = 'hello';
-let double: string = "hello";
-let back: string = `hello`;
+let single = 'hello';
+let double = "hello";
+let back = `hello`;
+
+let hello: string;
+hello = 'hello';
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Jack',
+  age: 21
+}
+
+const person2: object ={
+// const person2: {} ={
+  name: 'jack',
+  age: 21
+}
+// console.log(person2.name); //エラーになる
+
+const fruits: string[] = ['Apple', 'Banana', 'Grape'];
+const fruit = fruits[0];
+
+const book: [string, number, boolean] = ['business', 1500, false];
+// book.push(21);  // 追加時エラーは出ない
+// console.log(book[3]); //参照時にエラーをだす
+
+
+//sec2-18まで　19から
